5fdbde396d7e809fa119b5eac50d2b0636308cda
Daniel Ramon Monterrubio <872428@unizar.es>
2026-03-04 18:03:12 +0100

    Añadido join en RPC para obtener el nombre del organizador, corregido RLS en usuario y tipado/mappers para soportar organizerName con auto-login temporal en desarrollo. Tambien añadido automatizacion de lectura de bd para no tener que usar any

--- src/services/excursionService.ts
+++ src/services/excursionService.ts
@@ -1,10 +1,11 @@
 import { supabase } from "./supabaseClient";
 import { Excursion } from "../models/Excursion";
+import { mapExcursion } from "./mappers/excursionMapper";
 
 class ExcursionService {
   async getAvailableExcursions(): Promise<Excursion[]> {
     const { data, error } = await supabase.rpc("get_all_excursions");
-
+    console.log(data)
     if (error) {
       console.error(error);
       return [];
@@ -12,30 +13,7 @@ class ExcursionService {
 
     if (!data) return [];
 
-    return data.map((row: any) => {
-
-      const date = new Date(row.fechaInicio);
-
-      return {
-        id: row.id.toString(),
-        title: row.titulo,
-        type: row.tipoExcursion,
-        difficulty: row.dificultad,
-        date: date.toISOString().split("T")[0],
-        time: date.toTimeString().slice(0,5),
-        meetingPoint: row.puntoEncuentro,
-        meetingLat: Number(row.meetingLat),
-        meetingLng: Number(row.meetingLng),
-        organizerId: row.creadoPor,
-        organizerName: row.creadoPor,
-        capacity: row.capacidad,
-        acceptedCount: 2,
-        status: row.status,
-        imageUrl: row.imagenURL,
-        gpxPath: row.GPXPath,
-      };
-
-    });
+    return (data as any).map(mapExcursion)
   }
 }
 
